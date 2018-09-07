69b19874c9c1c50cbfc4c2062f78cb7b89ff67a1
Debora Dian Pratama <deboradian09@gmail.com>
2018-09-07 18:33:22 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,25 @@
+public class Mahasiswa{
+	private String nama;
+	private String npm;
+
+	public Mahasiswa(String nama, String npm){
+		this.nama = nama;
+		this.npm = npm;
+	}
+
+	public void setNama(String nama){
+		this.nama = nama;
+	}
+
+	public String getNama(){
+		return nama;
+	}
+
+	public void setNpm(String npm){
+		this.nama = nama;
+	}
+
+	public String getNpm(){
+		return npm;
+	}
+}
