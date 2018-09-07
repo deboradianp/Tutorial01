380686d608473d9d2cd4aa67abe088c70c18caad
Debora Dian Pratama <deboradian09@gmail.com>
2018-09-07 18:40:01 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,31 @@
+public class Dosen{
+	private String nama;
+	private String id_Dosen;
+	private int umur;
+
+	public Mahasiswa(String nama, String id_Dosen, int umur){
+		this.nama = nama;
+		this.npm = npm;
+		this.umur = umur;
+	}
+
+	public void setNama(String nama){
+		this.nama = nama;
+	}
+
+	public String getNama(){
+		return nama;
+	}
+
+	public void setNpm(String npm){
+		this.nama = nama;
+	}
+
+	public String getNpm(){
+		return npm;
+	}
+
+	public int getUmur(){
+		return umur;
+	}
+}
